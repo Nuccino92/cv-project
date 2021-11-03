5f7dd749c023780bbfc83d9e0b4ae2f8859425ba
Anthony Nucci <anthnucci@hotmail.com>
2021-11-03 01:39:31 -0400

    education form maker

---
+++ src/components/Main/Education/EducationForm.js
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+
+class EducationForm extends Component {
+  constructor() {
+    super();
+  }
+  render() {
+    return (
+      <form className="educationForm">
+        <label htmlFor="credentials">Rewarded Credentials</label>
+        <input
+          type="text"
+          id="credentials"
+          placeholder="Degree/Diploma etc"
+        ></input>
+
+        <label htmlFor="organization">Organization</label>
+        <input
+          type="text"
+          id="organization"
+          placeholder="University/College etc"
+        ></input>
+
+        <label htmlFor="fieldOfStudy">Field of Study</label>
+        <input
+          type="text"
+          id="fieldOfStudy"
+          placeholder="Short Description"
+        ></input>
+
+        <label htmlFor="startDate">Start:</label>
+        <input type="date" id="startDate"></input>
+
+        <label htmlFor="toDate">To:</label>
+        <input type="date" id="toDate"></input>
+      </form>
+    );
+  }
+}
+
+export default EducationForm;
