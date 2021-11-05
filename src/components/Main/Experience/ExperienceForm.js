03f7c1ec1590f67ad2c4d5823f149d258f113126
Nuccino92 <anthnucci@hotmail.com>
2021-11-05 02:35:57 -0400

    cleanup

--- src/components/Main/Experience/ExperienceForm.js
+++ src/components/Main/Experience/ExperienceForm.js
@@ -1,29 +1,24 @@
-import React, { Component } from "react";
+import React from "react";
 
-class ExperienceForm extends Component {
-  constructor() {
-    super();
-  }
-  render() {
-    return (
-      <form className="workForm">
-        <label htmlFor="title">Title</label>
-        <input type="text" id="title" placeholder="Job Name"></input>
+const ExperienceForm = () => {
+  return (
+    <form className="workForm">
+      <label htmlFor="title">Title</label>
+      <input type="text" id="title" placeholder="Job Name"></input>
 
-        <label htmlFor="employer">Employer</label>
-        <input type="text" id="employer" placeholder="Google"></input>
+      <label htmlFor="employer">Employer</label>
+      <input type="text" id="employer" placeholder="Google"></input>
 
-        <label htmlFor="startDate">Start:</label>
-        <input type="date" id="startDate"></input>
+      <label htmlFor="startDate">Start:</label>
+      <input type="date" id="startDate"></input>
 
-        <label htmlFor="toDate">To:</label>
-        <input type="date" id="toDate"></input>
+      <label htmlFor="toDate">To:</label>
+      <input type="date" id="toDate"></input>
 
-        <label htmlFor="jobDescription">Job Description</label>
-        <textarea id="jobDescription" rows="6"></textarea>
-      </form>
-    );
-  }
-}
+      <label htmlFor="jobDescription">Job Description</label>
+      <textarea id="jobDescription" rows="6"></textarea>
+    </form>
+  );
+};
 
 export default ExperienceForm;
