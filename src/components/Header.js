c4c2f2f34eb61bb02c5ffff3665578283b7930f8
Nuccino92 <anthnucci@hotmail.com>
2021-11-05 02:28:22 -0400

    extra styles, buttons, methods for buttons

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,78 @@
 import React from "react";
 
-function Header() {
+function Header({ handleActive }) {
+  function MouseOver(event) {
+    event.target.style.color = "#68CFCF";
+  }
+
+  function MouseOut(event) {
+    event.target.style.color = "#f54e42";
+  }
+
   return (
-    <header style={headerStyles}>
-      <div>Your CV</div>
+    <header style={HeaderStyles}>
+      <div>
+        Your CV
+        <div style={ButtonContainer}>
+          <button
+            onClick={handleActive}
+            onMouseOver={MouseOver}
+            onMouseOut={MouseOut}
+            style={ViewStyles}
+          >
+            View
+          </button>
+          <button
+            onMouseOver={MouseOver}
+            onMouseOut={MouseOut}
+            style={PDFStyles}
+          >
+            PDF
+          </button>
+        </div>
+      </div>
     </header>
   );
 }
 
-const headerStyles = {
+const HeaderStyles = {
+  position: "fixed",
+  top: 0,
+  width: "100%",
   boxShadow: "2px 2px 2px rgb(0 0 0 / 16%)",
+  backgroundColor: "#EEE6E7",
   color: "#f54e42",
   fontSize: "36px",
   fontFamily: "Arimo",
   fontWeight: "bolder",
   padding: "15px 15px 15px 110px",
+  zIndex: 10,
+};
+
+const ButtonContainer = {
+  float: "right",
+  marginRight: "100px",
+};
+
+const ViewStyles = {
+  cursor: "pointer",
+  fontFamily: "Arimo",
+  fontSize: "23px",
+  border: "none",
+  background: "none",
+  fontWeight: "bolder",
+  color: "#f54e42",
+  marginRight: "40px",
+};
+
+const PDFStyles = {
+  cursor: "pointer",
+  fontFamily: "Arimo",
+  fontSize: "23px",
+  border: "none",
+  background: "none",
+  fontWeight: "bolder",
+  color: "#f54e42",
 };
 
 export default Header;
