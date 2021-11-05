41f743d7d668672dc331b8daa5ea5841085e7c47
Nuccino92 <anthnucci@hotmail.com>
2021-11-05 02:27:05 -0400

    added state w/ handler for template

--- src/App.js
+++ src/App.js
@@ -6,12 +6,26 @@ import MainContent from "./components/Main/MainContent";
 class App extends Component {
   constructor() {
     super();
+
+    this.state = {
+      isActive: false,
+    };
+    this.handleActive = this.handleActive.bind(this);
   }
+
+  handleActive() {
+    console.log(this.state.isActive);
+    this.setState({ isActive: !this.state.isActive });
+  }
+
   render() {
     return (
       <div className="App">
-        <Header />
-        <MainContent />
+        <Header handleActive={this.handleActive} />
+        <MainContent
+          handleActive={this.handleActive}
+          isActive={this.state.isActive}
+        />
       </div>
     );
   }
