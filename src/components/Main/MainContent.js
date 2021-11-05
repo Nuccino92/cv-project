fbf367b4d5a8c195abe6d5df8f801005fc3f3c41
Nuccino92 <anthnucci@hotmail.com>
2021-11-05 02:29:55 -0400

    added state, template also passed states/methods

--- src/components/Main/MainContent.js
+++ src/components/Main/MainContent.js
@@ -3,18 +3,46 @@ import PersonalInformation from "./PersonalInformation";
 import Experience from "./Experience/Experience";
 import Education from "./Education/Education";
 import Skills from "./Skills/Skills";
+import Template from "./CVTemplate/CVTemplate";
 
 class MainContent extends Component {
   constructor() {
     super();
+
+    this.state = {
+      firstName: "",
+      lastName: "",
+      profession: "",
+      city: "",
+      province: "",
+      phone: "",
+      email: "",
+    };
+
+    this.handleChange = this.handleChange.bind(this);
   }
+
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  }
+
   render() {
+    const handleActive = this.props.handleActive;
+    const { isActive } = this.props;
+
     return (
       <div style={ContainerStyle} className="mainContainer">
-        <PersonalInformation />
+        <PersonalInformation handleChange={this.handleChange} />
         <Experience />
         <Education />
         <Skills />
+        <Template
+          state={this.state}
+          isActive={isActive}
+          handleActive={handleActive}
+        />
       </div>
     );
   }
@@ -22,6 +50,8 @@ class MainContent extends Component {
 
 const ContainerStyle = {
   padding: "40px",
+  marginTop: "150px",
+  position: "relative",
 };
 
 export default MainContent;
