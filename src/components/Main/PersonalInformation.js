7f8c777122c69757899092772fdf739247b18ae2
Nuccino92 <anthnucci@hotmail.com>
2021-11-05 02:31:20 -0400

    added jsx

--- src/components/Main/PersonalInformation.js
+++ src/components/Main/PersonalInformation.js
@@ -1,10 +1,6 @@
 import React, { Component } from "react";
 
 class PersonalInformation extends Component {
-  constructor() {
-    super();
-  }
-
   render() {
     return (
       <div className="personalInformationContainer">
@@ -15,30 +11,86 @@ class PersonalInformation extends Component {
             <label htmlFor="firstName">First Name</label>
             <label htmlFor="lastName">Last Name</label>
 
-            <input type="text" id="firstName" placeholder="John" />
-            <input type="text" id="lastName" placeholder="Doe" />
+            <input
+              type="text"
+              name="firstName"
+              id="firstName"
+              maxLength="11"
+              placeholder="John"
+              onChange={this.props.handleChange}
+            />
+            <input
+              type="text"
+              name="lastName"
+              id="lastName"
+              maxLength="11"
+              placeholder="Doe"
+              onChange={this.props.handleChange}
+            />
           </div>
 
-          <label htmlFor="profession">Profession</label>
-          <input type="text" id="profession" placeholder="Carpenter" />
+          <label htmlFor="profession" maxLength="10">
+            Profession
+          </label>
+          <input
+            type="text"
+            name="profession"
+            id="profession"
+            placeholder="Carpenter"
+            onChange={this.props.handleChange}
+          />
 
           <div className="pairContainer">
             <label htmlFor="city">City</label>
             <label htmlFor="province">Province/State</label>
 
-            <input type="text" id="city" placeholder="Toronto" />
-            <input type="text" id="province" placeholder="Ontario" />
+            <input
+              type="text"
+              name="city"
+              id="city"
+              placeholder="Toronto"
+              onChange={this.props.handleChange}
+            />
+            <input
+              type="text"
+              name="province"
+              id="province"
+              placeholder="Ontario"
+              onChange={this.props.handleChange}
+            />
           </div>
 
           <div className="pairContainer">
-            <label htmlFor="phone">Phone Number</label>
-            <label htmlFor="email">Email Adress</label>
+            <label htmlFor="phone" name="phone">
+              Phone Number
+            </label>
+            <label htmlFor="email" name="email">
+              Email Adress
+            </label>
 
-            <input type="text" id="phone" placeholder="(123) 456-7899" />
-            <input type="text" id="email" placeholder="Bob123@hotmail.com" />
+            <input
+              type="text"
+              name="phone"
+              id="phone"
+              placeholder="(123) 456-7899"
+              onChange={this.props.handleChange}
+            />
+            <input
+              type="text"
+              name="email"
+              id="email"
+              placeholder="John123@hotmail.com"
+              onChange={this.props.handleChange}
+            />
           </div>
 
-          <label htmlFor="photo">Photo</label>
+          <label
+            htmlFor="photo"
+            name="photo"
+            onChange={this.props.handleChange}
+          >
+            Photo
+          </label>
           <input type="file" id="photo" />
         </form>
       </div>
